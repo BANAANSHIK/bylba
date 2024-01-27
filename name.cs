d1b08060d52a06c847aeb548e618bfaa55f80df1
BANAANSHIK <157865100+BANAANSHIK@users.noreply.github.com>
2024-01-27 14:51:17 +1000

    Create name.cs

---
+++ name.cs
@@ -0,0 +1,19 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+    int zo = 10;
+    if (zo <= 18){
+    Console.WriteLine(" ты не можещь кипить яйцо");
+    }
+    else { 
+        Console.WriteLine("молодуц");
+    }
+    for(int i = 0;i < 100; i++ )
+        Console.WriteLine("gghhh" + i);
+int a = 10;
+while (a <= 100) {
+        Console.WriteLine("fn" + a);
+        a = a + 100;
+  }
+}
